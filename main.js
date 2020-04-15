b605f7ecc6530798bd41f720bbdc32c6704d4f55
legenddaniel <siyuanincanada@gmail.com>
2020-04-14 21:18:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import { carousel, setSectionHeight, goTop, scrollCell, getProvince, btnGet1, btnGet2, showResult1, showResult2, scrollPage, btnArrow, position, scrollEnd, wheelEnd, preloadImg, scrollCellMobile, touchmoveMobile, restorePage} from './utils.js';
+import { carousel, setSectionHeight, goTop, scrollCell, getProvince, btnGet1, btnGet2, showResult1, showResult2, scrollPage, btnArrow, position, scrollEnd, wheelEnd, preloadImg, scrollCellMobile, touchmoveMobile, restorePage } from './utils.js';
 // import smoothscroll from 'smoothscroll-polyfill';
- 
+
 // smoothscroll.polyfill();
 
 const imgs = ['img/bc.jpg', 'img/mn.jpg', 'img/nb.jpg', 'img/nl.jpg', 'img/ns.jpg', 'img/on.jpg', 'img/pe.jpg', 'img/qc.jpg', 'img/sk.jpg'];
@@ -9,12 +9,11 @@ window.addEventListener('load', preloadImg(...imgs));
 window.addEventListener('scroll', scrollEnd);
 window.addEventListener('beforeunload', goTop);
 window.addEventListener('resize', restorePage);
-// if (isMobile()) {
-    window.addEventListener('load', setSectionHeight);
-    window.addEventListener('resize', setSectionHeight);
-// }
-// window.addEventListener('scroll', e => e.preventDefault());
-// window.addEventListener('touchmove', e => e.preventDefault());
+window.addEventListener('load', setSectionHeight);
+window.addEventListener('resize', setSectionHeight);
+
+window.addEventListener('scroll', e => e.preventDefault());
+window.addEventListener('touchmove', e => e.preventDefault());
 
 carousel.addEventListener('wheel', scrollCell);
 // carousel.addEventListener('wheel', getProvince);
