10762279819379f37df03e08b99aa4e784db6276
legenddaniel <siyuanincanada@gmail.com>
2020-04-17 18:48:30 -0400

    Update utils.js

--- utils.js
+++ utils.js
@@ -56,7 +56,7 @@ const getRotateDeg = () => {
 
 export const setSectionHeight = debounce(() => {
     const match = window.matchMedia || window.msmatch;
-    const mobile = !match("(hover)").matches;
+    const mobile = match("pointer:coarse").matches;
     if (mobile) {
         const vh = window.innerHeight / 100;
         document.documentElement.style.setProperty('--vh', `${vh}px`);
